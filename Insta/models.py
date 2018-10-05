1f7a82657098091b1ce2b603007e3bcb82af0abf
Protus bantan <protus.bantan@gmail.com.com>
2018-10-05 18:53:55 +0300

    added Image Model

--- Insta/models.py
+++ Insta/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    Image=models.ImageField(upload_to='Images/')
+    caption = models.TextField(blank=True)
+    likes = models.PositiveIntegerField(default=0)
+    time = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE)
