d5980ad23916419f001f1c7346c3e99b0b4721eb
protus <protus.bantan@gmail.com>
2019-05-17 16:42:06 +0300

    created a superuser

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/protus/Instagram/virtual"
+set -gx VIRTUAL_ENV "/home/protus/Documents/Instagram/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
